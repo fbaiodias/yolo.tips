3aae3164906af7a0fbf93245d866ac93d9729a42
Francisco Baio Dias <xicombd@gmail.com>
2015-09-13 21:56:57 +0100

    Displaying results

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import { ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap'
 import search from '../services/search'
 
 export default class Results extends Component {
@@ -25,10 +26,29 @@ export default class Results extends Component {
   }
 
   render () {
+    const results = this.state.results ? this.state.results.map((result) => (
+      <ListGroupItem>
+        <a href={result.url}>
+          <Row>
+            <Col xs={10} md={10}>
+              <b>{result.outbound.to.city}, {result.outbound.to.country}</b>
+              <p>{result.outbound.from.airport}, {result.outbound.from.country} to {result.outbound.to.airport}, {result.outbound.to.country} - {result.outbound.departureDate}</p>
+              <p>{result.inbound.from.airport}, {result.inbound.from.country} to {result.inbound.to.airport}, {result.inbound.to.country} - {result.inbound.departureDate}</p>
+            </Col>
+            <Col xs={2} md={2}>
+              {result.price} {result.currencyId}
+            </Col>
+          </Row>
+        </a>
+      </ListGroupItem>
+    )) : (
+      <h3>loading...</h3>
+    )
+
     return (
-      <div className='container'>
-        {this.state.results}
-      </div>
+      <ListGroup>
+        {results}
+      </ListGroup>
     )
   }
 }
